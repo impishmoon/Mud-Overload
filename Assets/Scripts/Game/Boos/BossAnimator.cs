3ad2f1095d76d511b4a6dcc013d24be7091f875f
ZeroByter <romgamesrom@gmail.com>
2023-02-03 21:25:57 +0200

    more boss fight work

--- Assets/Scripts/Game/Boos/BossAnimator.cs
+++ Assets/Scripts/Game/Boos/BossAnimator.cs
@@ -2,70 +2,147 @@ using UnityEngine;
 
 namespace MudOverload.Game.Boos
 {
-	public class BossAnimator : MonoBehaviour
-	{
-		public enum CurrentAnimation
+    public class BossAnimator : MonoBehaviour
+    {
+        [Header("Others")]
+        [SerializeField]
+        private float animationLerpSpeed = 1;
+        [SerializeField]
+        private float animationSpeed = 7.5f;
+        [SerializeField]
+        private float movementSpeed = 5f;
+        [SerializeField]
+        private float displaySpeedLerp = 10;
+
+        #region Inputs
+        [Header("Inputs")]
+        [SerializeField]
+        private Transform upper;
+        [SerializeField]
+        private Transform head;
+        [SerializeField]
+        private Transform chest;
+        [SerializeField]
+        private Transform leftShoulder;
+        [SerializeField]
+        private Transform leftArm;
+        [SerializeField]
+        private Transform leftHand;
+        [SerializeField]
+        private Transform rightShoulder;
+        [SerializeField]
+        private LineRenderer rightShoulderLineRenderer;
+        [SerializeField]
+        private Transform rightArm;
+        [SerializeField]
+        private Transform rightHand;
+        [SerializeField]
+        private Transform pp;
+        [SerializeField]
+        private Transform leftKnee;
+        [SerializeField]
+        private Transform leftFoot;
+        [SerializeField]
+        private Transform rightKnee;
+        [SerializeField]
+        private Transform rightFoot;
+        #endregion
+
+        private SpriteRenderer leftFootRenderer;
+        private SpriteRenderer rightFootRenderer;
+        private SpriteRenderer headRenderer;
+        private SpriteRenderer chestRenderer;
+
+        private TransformKeeper[] keepers;
+
+        private Vector2 lastPosition;
+
+        private float displaySpeed = 0;
+
+        private float sleep = 0;
+
+        //Firing animation data
+        private enum FiringStage
         {
-			IDLE,
+            NONE,
+            AIMING,
+            SHOOTING,
         }
-
-		[Header("Others")]
-		[SerializeField]
-		private float speed = 2f;
-
-		#region Inputs
-		[Header("Inputs")]
-		[SerializeField]
-		private Transform upper;
-		[SerializeField]
-		private Transform head;
-		[SerializeField]
-		private Transform leftShoulder;
-		[SerializeField]
-		private Transform leftArm;
-		[SerializeField]
-		private Transform leftHand;
-		[SerializeField]
-		private Transform rightShoulder;
-		[SerializeField]
-		private Transform rightArm;
-		[SerializeField]
-		private Transform rightHand;
-		[SerializeField]
-		private Transform pp;
-		[SerializeField]
-		private Transform leftKnee;
-		[SerializeField]
-		private Transform leftFoot;
-		[SerializeField]
-		private Transform rightKnee;
-		[SerializeField]
-		private Transform rightFoot;
-		#endregion
-
-		private TransformKeeper[] keepers;
+        private FiringStage firingStage = FiringStage.NONE;
+        private Vector2 firingTarget = new Vector2(7.5f, -5);
 
         private void Awake()
         {
-			keepers = GetComponentsInChildren<TransformKeeper>();
+            keepers = GetComponentsInChildren<TransformKeeper>();
 
-		}
+            leftFootRenderer = leftFoot.GetComponent<SpriteRenderer>();
+            rightFootRenderer = rightFoot.GetComponent<SpriteRenderer>();
+            headRenderer = head.GetComponent<SpriteRenderer>();
+            chestRenderer = chest.GetComponent<SpriteRenderer>();
+        }
 
         private void Update()
         {
-			var animation = CurrentAnimation.IDLE;
+            //calculating movement speed
+            transform.position = new Vector3(Mathf.Sin(Time.time) * (10f * (1 - sleep)), 0, 0);
+            var positionDifference = (Vector2)transform.position - lastPosition;
 
-			if(animation == CurrentAnimation.IDLE)
+            //lerping display movement speed to prevent jitterness
+            displaySpeed = Mathf.Lerp(displaySpeed, positionDifference.x, displaySpeedLerp * Time.deltaTime);
+
+            //flipping what needs to be flipped depending on which direction we are moving
+            leftFootRenderer.flipX = positionDifference.x < 0;
+            rightFootRenderer.flipX = positionDifference.x < 0;
+            headRenderer.flipX = positionDifference.x < 0;
+            chestRenderer.flipX = positionDifference.x < 0;
+
+            //bobbing up and down
+            upper.localPosition = new Vector3(Mathf.Sin(Time.time * animationSpeed) * (0.01f * (1 - sleep)), Mathf.Cos(Time.time * animationSpeed) * (0.05f * (1 - sleep)));
+
+            //head facing down when sleeping
+            head.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(head.localEulerAngles.z, sleep * (headRenderer.flipX ? 45 : -45), animationLerpSpeed * Time.deltaTime));
+
+            //moving knees and legs when we are moving
+            rightKnee.localEulerAngles = new Vector3(0, 0, Mathf.Sin(Time.time * animationSpeed) * 45 * displaySpeed * movementSpeed);
+            rightFoot.rotation = Quaternion.identity;
+            leftKnee.localEulerAngles = new Vector3(0, 0, Mathf.Cos(Time.time * animationSpeed) * 45 * displaySpeed * movementSpeed);
+            leftFoot.rotation = Quaternion.identity;
+
+            //storing lastPosition for figuring out movement speed later on
+            lastPosition = transform.position;
+
+            if (firingStage == FiringStage.NONE)
             {
-				upper.localPosition = new Vector3(Mathf.Sin(Time.time * speed) * 0.025f, Mathf.Cos(Time.time * speed) * 0.25f);
+                rightShoulder.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(rightShoulder.localEulerAngles.z, 0, animationLerpSpeed * Time.deltaTime));
+                rightArm.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(rightArm.localEulerAngles.z, 0, animationLerpSpeed * Time.deltaTime));
+                rightHand.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(rightHand.localEulerAngles.z, 0, animationLerpSpeed * Time.deltaTime));
+
+                rightHand.localPosition = Vector3.Lerp(rightHand.localPosition, new Vector3(0.07000017f, -1.28f), animationLerpSpeed * Time.deltaTime);
+                rightArm.localPosition = Vector3.Lerp(rightHand.localPosition, new Vector3(0.02999997f, -1.48f), animationLerpSpeed * Time.deltaTime);
             }
-		}
-
-		private void ResetAll()
-        {
-			foreach(var keeper in keepers)
+            else
             {
-				keeper.Reset();
+                var direction = (firingTarget - (Vector2)rightShoulder.position).normalized;
+                rightShoulder.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(rightShoulder.localEulerAngles.z, Mathf.Atan2(direction.x, direction.y) * -Mathf.Rad2Deg + 180, animationLerpSpeed * Time.deltaTime));
+
+                if (firingStage == FiringStage.SHOOTING)
+                {
+                    rightHand.localPosition = Vector3.Lerp(rightHand.localPosition, rightArm.InverseTransformPoint(firingTarget), animationLerpSpeed * Time.deltaTime);
+                    rightArm.localPosition = Vector3.Lerp(rightHand.localPosition, rightShoulder.InverseTransformPoint(((Vector2)rightShoulder.position + firingTarget) / 2), animationLerpSpeed * Time.deltaTime);
+                }
+            }
+
+            rightShoulderLineRenderer.SetPositions(new Vector3[] {
+                rightShoulder.position,
+                rightHand.TransformPoint(new Vector3(0,0.5f,0)),
+            });
+        }
+
+        private void ResetAll()
+        {
+            foreach (var keeper in keepers)
+            {
+                keeper.Reset();
             }
         }
     }
