1c0f4f0e75213c45a6dac621c5b19a60ce379407
ZeroByter <romgamesrom@gmail.com>
2023-02-04 15:37:15 +0200

    uploadded new animations

---
+++ Assets/Scripts/Game/Player/PlayerAnimator.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+namespace MudOverload.Game.Player
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite[] idleSprites;
+        [SerializeField]
+        private Sprite[] runningSprites;
+        [SerializeField]
+        private Sprite[] miningSprites;
+
+        private new SpriteRenderer renderer;
+        private Rigidbody2D rb;
+
+        private int frameIndex = 0;
+        private int animationIndex = 0;
+        private int lastAnimationIndex = 0;
+        private float lastChangeFrame = 0;
+
+        [SerializeField]
+        private float animationSpeed = 0.1f;
+
+        private Vector2 lastPosition;
+
+        private bool isMining = false;
+
+        private void Awake()
+        {
+            renderer = GetComponent<SpriteRenderer>();
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            var diff = lastPosition - rb.position;
+            var hspeed = diff.x;
+
+            print(diff);
+
+            if (isMining)
+            {
+                animationIndex = 2;
+                ResetFrameIndexIfNeeded();
+
+                if (Time.time > lastChangeFrame + animationSpeed) AdvanceFrameIndex(miningSprites);
+            }
+            else if (hspeed != 0)
+            {
+                animationIndex = 1;
+                ResetFrameIndexIfNeeded();
+
+                if (Time.time > lastChangeFrame + animationSpeed) AdvanceFrameIndex(runningSprites);
+            }
+            else
+            {
+                animationIndex = 0;
+                ResetFrameIndexIfNeeded();
+
+                if (Time.time > lastChangeFrame + animationSpeed) AdvanceFrameIndex(idleSprites);
+            }
+
+            lastPosition = rb.position;
+            lastAnimationIndex = animationIndex;
+        }
+
+        private void ResetFrameIndexIfNeeded()
+        {
+            if(animationIndex != lastAnimationIndex)
+            {
+                frameIndex = 0;
+            }
+        }
+
+        private void AdvanceFrameIndex(Sprite[] sprites)
+        {
+            frameIndex++;
+            if(frameIndex > sprites.Length - 1) frameIndex = 0;
+            renderer.sprite = sprites[frameIndex];
+            lastChangeFrame = Time.time;
+        }
+    }
+}
